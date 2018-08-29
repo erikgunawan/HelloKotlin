efb232c825e21d0a6ab543b827540e0158593896
erikgunawan <erik.gunawan44@gmail.com>
2018-08-29 21:25:23 +0700

    Kotlin Android Extensions

--- app/src/main/java/id/ergun/hellokotlin/Item.kt
+++ app/src/main/java/id/ergun/hellokotlin/Item.kt
@@ -1,3 +1,7 @@
 package id.ergun.hellokotlin
 
-data class Item (val name: String?, val image: Int?)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Item (val name: String?, val image: Int?) : Parcelable
--- app/src/main/java/id/ergun/hellokotlin/MainActivity.kt
+++ app/src/main/java/id/ergun/hellokotlin/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,14 +16,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val textHello = findViewById<TextView>(R.id.text_hello)
-//        textHello.text = "Hello Kotlin!"
+        text_name.text = "Hello Kotlin"
 
-        val list = findViewById<RecyclerView>(R.id.club_list)
         initData()
 
-        list.layoutManager = LinearLayoutManager(this)
-        list.adapter = RecyclerViewAdapter(this, items){
+        club_list.layoutManager = LinearLayoutManager(this)
+        club_list.adapter = RecyclerViewAdapter(this, items){
             val toast = Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT)
             toast.show()
         }
